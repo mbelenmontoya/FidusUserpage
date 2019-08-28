64fec2fc1ad0f1d1274041c29c7c7357074ea101
Belen montoya <belenmontoya@MacBook-Pro-de-Belen.local>
2019-08-28 01:41:48 -0300

    Updates

---
+++ precache-manifest.82991cef011015a8e6b85ecb38adfbe9.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d730f60c38531a57eb10daf33bacb6c",
+    "url": "/FidusUserpage/index.html"
+  },
+  {
+    "revision": "c0fc530ead51d50badce",
+    "url": "/FidusUserpage/static/css/main.6f786156.chunk.css"
+  },
+  {
+    "revision": "8064b342dcb15364f69c",
+    "url": "/FidusUserpage/static/js/2.2f6d7583.chunk.js"
+  },
+  {
+    "revision": "c0fc530ead51d50badce",
+    "url": "/FidusUserpage/static/js/main.451f99c4.chunk.js"
+  },
+  {
+    "revision": "f7cff4f4590d452e9c82",
+    "url": "/FidusUserpage/static/js/runtime~main.0474e0bd.js"
+  },
+  {
+    "revision": "4d5b96f7f58dbb32d9ac1662b154d58f",
+    "url": "/FidusUserpage/static/media/appstore@2x.4d5b96f7.jpg"
+  },
+  {
+    "revision": "752fc4bbbbaf38561c025da7b8300e73",
+    "url": "/FidusUserpage/static/media/bg1@2x.752fc4bb.jpg"
+  },
+  {
+    "revision": "7c5f999975f806fec270499d73e9aae9",
+    "url": "/FidusUserpage/static/media/fidusapp.7c5f9999.png"
+  },
+  {
+    "revision": "b9ec591b955525505a4de727e5da0f00",
+    "url": "/FidusUserpage/static/media/googleplay@2x.b9ec591b.jpg"
+  },
+  {
+    "revision": "1d6751af8cc943ca5e7a3af0d809be0e",
+    "url": "/FidusUserpage/static/media/horizontal@2x.1d6751af.png"
+  },
+  {
+    "revision": "17cc9cd9185f9adf49d1c2f7adca28ae",
+    "url": "/FidusUserpage/static/media/imagenbanner.17cc9cd9.png"
+  },
+  {
+    "revision": "12f9dd1fc107bfa18d79198a6795e074",
+    "url": "/FidusUserpage/static/media/logonews.12f9dd1f.png"
+  },
+  {
+    "revision": "37962e3f54a61bea3562fd04571d0a16",
+    "url": "/FidusUserpage/static/media/newsimage.37962e3f.jpg"
+  },
+  {
+    "revision": "5ab18564bf4eedf6685d85882a96d31a",
+    "url": "/FidusUserpage/static/media/noimage.5ab18564.png"
+  },
+  {
+    "revision": "1d01527b145b65b9eb99b97867a665eb",
+    "url": "/FidusUserpage/static/media/vertical@2x.1d01527b.png"
+  }
+]);
